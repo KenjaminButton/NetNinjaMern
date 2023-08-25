f55eedad13dba69fb6e75fb12405bbb08403dd96
KenjaminButton <kennethpchang@gmail.com>
2023-08-26 01:41:20 +0800

    Express App setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // Express app
 const app = express()
 
+// Global Middleware
+app.use( (req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.get('/', (req, res) => {
+  res.json({msg: 'Welcome to our MERN NETNINJA APP!'})
+})
+
 // Listen for requests
-app.listen(4000, () => {
-  console.log('listening on port 4000 right now')
+app.listen(process.env.PORT, () => {
+  console.log('listening on port 4000 on a Friday')
 })
 
